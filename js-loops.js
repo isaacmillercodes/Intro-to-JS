e535f9c932473097fa90cc7cbf21ddc09a382d9f
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2016-07-11 11:56:02 -0600

    finished first 7 problems of JS Loops exercise

---
+++ js-loops.js
@@ -0,0 +1,45 @@
+console.log("Problem 1");
+
+for (var i = 0; i <= 1000; i += 100) {
+  console.log(i);
+}
+
+console.log("Problem 2");
+
+for (var i = 1; i <= 128; i *= 2) {
+  console.log(i);
+}
+
+console.log("Problem 3");
+
+for (var i = 0; i <= 10; i += 2) {
+  console.log(i);
+}
+
+console.log("Problem 4");
+
+for (var i = 3; i <= 15; i += 3) {
+  console.log(i);
+}
+
+console.log("Problem 5");
+
+for (var i = 9; i >= 0; i--) {
+  console.log(i);
+}
+
+console.log("Problem 6");
+
+for (var i = 1; i <= 4; i++) {
+  console.log(i);
+  console.log(i);
+  console.log(i);
+}
+
+console.log("Problem 7");
+
+for (var n = 0; n < 4; n++) {
+  for (var i = 0; i <= 4; i++) {
+    console.log(i);
+  };
+}
